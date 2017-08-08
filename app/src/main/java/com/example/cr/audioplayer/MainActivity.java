692f4d7cd92d7a73672dbe3b23328885aa14db12
shanker <vennamshanker@gmail.com>
2017-08-07 21:51:44 -0400

    Added classes:MediaPlayerService and StorageUtil

--- app/src/main/java/com/example/cr/audioplayer/MainActivity.java
+++ app/src/main/java/com/example/cr/audioplayer/MainActivity.java
@@ -3,7 +3,12 @@ package com.example.cr.audioplayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    public static final int REQUEST_ID_MULTIPLE_PERMISSIONS =1;
+    public static final String Broadcast_PLAY_NEW_AUDIO = "com.example.cr.audioplayer.PlayNewAudio";
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
